96fdef123d9ea39c8b53c63650104ea903e13ac5
kannadamoviesrequestbot <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2021-08-16 12:03:09 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"<i>🕹 CHECK RESULTS AT @KANNADAMOVIES_17 🕹..Found</i> <code>{leng}</code> <i>Results For Your Requested movie :</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
